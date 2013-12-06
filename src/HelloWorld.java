3f26b67209d2346624227b6c85a0f489ac743bac
hhxytingzw <hhxytingzw>
2013-12-06 16:35:56 +0800

    偷偷www

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,12 @@
-
 public class HelloWorld {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
